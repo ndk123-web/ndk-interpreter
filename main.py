19274d2ef74576dd886ab77d4a581e96a9014233
Navnath Kadam <navnathkadam284@gmail.com>
2026-07-20 13:28:45 +0530

    feat: added release and cmd support like now we can do like "ndk run file_path_.ndk"

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from lexer.lexer import lexer
 from parse.parser import parser
 from interpreter_ndk.interpreter import InterpreterNdk
+import sys
 
 if __name__ == "__main__":
 
-    with open("examples/example.ndk", "r") as f:
-        source_code = f.read()
-        tokens = lexer(source_code=source_code)
-        # print("Tokens: ", tokens)
+    if len(sys.argv) > 1:
 
-        
-        ast = parser(tokens)
-        # print("AST: ", ast)
+        # might be run
+        command = sys.argv[1]
 
+        # get file path/ file name in current directory
+        file_path = sys.argv[2]
 
-        ipr = InterpreterNdk()
-        ipr.run(ast)
+        if not file_path.endswith(".ndk"):
+            print("Error: File must have a .ndk extension")
+            sys.exit(1)
 
-        # print() 
-        # print() 
-        # print() 
-        # print() 
-        # print("Global Scope: ", ipr.get_global_scope())
+        if command == "run" and len(sys.argv) == 3:
+
+            with open(file_path, "r") as f:
+
+                source_code = f.read()
+                tokens = lexer(source_code=source_code)
+                # print("Tokens: ", tokens)
+
+                ast = parser(tokens)
+                # print("AST: ", ast)
+
+                ipr = InterpreterNdk()
+                ipr.run(ast)
+
+                # print()
+                # print()
+                # print()
+                # print()
+                # print("Global Scope: ", ipr.get_global_scope())
